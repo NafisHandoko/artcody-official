caba2d52664e13f5aaf7642e193adeff02a3fc9b
Nafis Handoko <naffisioner@gmail.com>
2022-03-07 19:50:11 +0700

    footer basic style, nav change color on scroll, and vertical line done

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var height = $(document).height();
+    $('#vertical-line').css('height', `${0.94*height}px`)
+})
+
+$(window).resize(function(){
+    var height = $(document).height(); // or $(window).height()
+});
+
+$(document).scroll(function () {
+    var $nav = $("#nav");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
