ca54ca149db4c3a2e2675032f0c6b2e79f6a4d9d
Nafis Handoko <naffisioner@gmail.com>
2022-03-08 16:06:53 +0700

    change color on nav link when scroll reach specific id

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var height = $(document).height();
     $('#vertical-line').css('height', `${0.94*height}px`)
+    console.log(document.getElementsByTagName('section'))
+    console.log()
 })
 
 $(window).resize(function(){
@@ -10,6 +12,22 @@ $(window).resize(function(){
 $(document).scroll(function () {
     var $nav = $("#nav");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+    var current = "";
+    var sections = document.querySelectorAll('.navlink-target')
+    var navLi = document.querySelectorAll('.navlink-li')
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (scrollY >= sectionTop ) {
+        current = section.getAttribute("id"); }
+    });
+  
+    navLi.forEach((li) => {
+      li.classList.remove("navlink-active");
+      if (li.classList.contains(current)) {
+        li.classList.add("navlink-active");
+      }
+    });
 });
 
 var typed = new Typed('#role-text', {
@@ -18,3 +36,20 @@ var typed = new Typed('#role-text', {
     typeSpeed: 50,
     backSpeed: 50
 });
+
+// window.onscroll = () => {
+//     var current = "";
+//     $('section').forEach((section) => {
+//       const sectionTop = section.offsetTop;
+//       if (scrollY >= sectionTop ) {
+//         current = section.getAttribute("id"); }
+//     });
+  
+//     $('li').forEach((li) => {
+//       li.classList.remove("navlink-active");
+//       if (li.classList.contains(current)) {
+//         li.classList.add("navlink-active");
+//       }
+//     });
+//   };
+  
